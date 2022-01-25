143eb3fa4937a42b98fd9b8ead5039fe7ff1f62c
Emily <porte2@mailbox.winthrop.edu>
2022-01-25 10:26:28 -0500

    Started working on getting the login created

--- charm-tracker/imports/ui/App.js
+++ charm-tracker/imports/ui/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     render() {
         return (
             <>
-                <Title title={this.props.passedPropTitle} />
+                {/* <Title title={this.props.passedPropTitle} /> */}
                 <Login />
             </>
         );
