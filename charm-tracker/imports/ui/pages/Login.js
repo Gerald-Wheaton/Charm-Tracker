ec69174c7863dfc09226070f42c2dddac5865189
Emily <porte2@mailbox.winthrop.edu>
2022-02-03 10:26:53 -0500

    created page directory for finished page components

--- charm-tracker/imports/ui/pages/Login.js
+++ charm-tracker/imports/ui/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import LoginHeader from './LoginHeader';
+import LoginHeader from '../LoginHeader';
 
 export default class Login extends React.Component {
     render() { // OAuth URI is not compatable with localhost:3000
