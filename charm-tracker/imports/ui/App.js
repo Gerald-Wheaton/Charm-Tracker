a99c7d775dc8dbaaff009a56b6469d7af27a469b
Gerald-Wheaton <geraldwheaton2000@gmail.com>
2022-02-15 19:23:09 -0500

    add simple authentication. Now only render login page or nav menu

--- charm-tracker/imports/ui/App.js
+++ charm-tracker/imports/ui/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 // import { UP_Collection_Access } from './../api/user_posts.js';
 import Title from "./Title.js"
 import Login from "./pages/Login.js"
@@ -6,22 +6,19 @@ import ResetPassword from "./pages/ResetPassword.js"
 import PropTypes from "prop-types"
 import PageRoutes from "./PageRoutes.js"
 
-//export default class App extends React.Component {
-//render() {
 const App = props => {
   const { title } = props
+  const [loggedIn, setLoggedIn] = useState()
+
+  if (!loggedIn) {
+    return <Login setLoggedIn={setLoggedIn} />
+  }
   return (
     <>
       <PageRoutes />
       {/* <Title title={this.props.passedPropTitle} /> */}
-      <Login />
     </>
   )
 }
-//}
-
-// App.propTypes = {
-//   passedPropTitle: PropTypes.string.isRequired,
-// }
 
 export default App
