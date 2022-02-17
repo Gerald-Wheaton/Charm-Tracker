88d4c7b8a3874b7cc521592e94e9ce68b3a391b1
Emily <porte2@mailbox.winthrop.edu>
2022-02-17 18:58:20 -0500

    added the form bells and whistles

--- charm-tracker/imports/ui/ContactDetailsForm.js
+++ charm-tracker/imports/ui/ContactDetailsForm.js
@@ -1,47 +1,63 @@
-import React from "react"
+import React from "react";
 
-const ContactDetailsForm = () => {
-    return (
-        <div>
-            <form>
-                <fieldset>
-                    <legend>Contact Details</legend>
-                    <div>
-                        <label for="fname">First Name</label><br />
-                        <input type="input" id="fname" name="fname"></input>
-                    </div>
-                    <div>
-                        <label for="lname">Last Name</label><br />
-                        <input type="input" id="lname" name="lname"></input>
-                    </div>
-                    <div>
-                        <label for="email">Email</label><br />
-                        <input type="email" id="email" name="email"></input>
-                    </div>
-                    <div>
-                        <label for="phoneNum">Phone Number</label><br />
-                        <input type="input" id="PhoneNum" name="PhoneNum"></input>
-                    </div>
-                    <div>
-                        <label for="address">Address</label><br />
-                        <input type="input" id="address" name="address"></input>
-                    </div>
-                    <div>
-                        <label for="city">City</label><br />
-                        <input type="input" id="city" name="city"></input>
-                    </div>
-                    <div>
-                        <label for="state">State</label><br />
-                        <input type="input" id="state" name="state"></input>
-                    </div>
-                    <div>
-                        <label for="zip">Zip</label><br />
-                        <input type="input" id="zip" name="zip"></input>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    )
-}
+const ContactDetailsForm = (props) => {
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <legend>Contact Details</legend>
+          <div>
+            <label>
+              First Name
+              <input type="input" id="fname" name="fname" value={props.previousCustomer}></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Last Name
+              <input type="input" id="lname" name="lname"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Email
+              <input type="email" id="email" name="email"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Phone Number
+              <input type="input" id="PhoneNum" name="PhoneNum"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Address
+              <input type="input" id="address" name="address"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              City
+              <input type="input" id="city" name="city"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              State
+              <input type="input" id="state" name="state"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Zip
+              <input type="input" id="zip" name="zip"></input>
+            </label>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
 
-export default ContactDetailsForm
+export default ContactDetailsForm;
--- charm-tracker/imports/ui/EventDetailsForm.js
+++ charm-tracker/imports/ui/EventDetailsForm.js
@@ -1,31 +1,39 @@
-import React from "react"
+import React from "react";
 
 const EventDetailsForm = () => {
-    return (
-        <div>
-            <form>
-                <fieldset>
-                    <legend>Event Details</legend>
-                    <div>
-                        <label for="date">Date</label><br />
-                        <input type="date" id="date" name="date"></input>
-                    </div>
-                    <div>
-                        <label for="startTime">Start Time</label><br />
-                        <input type="time" id="startTime" name="startTime"></input>
-                    </div>
-                    <div>
-                        <label for="stopTime">Start Time</label><br />
-                        <input type="time" id="stopTime" name="stopTime"></input>
-                    </div>
-                    <div>
-                        <label for="price">Price of Event</label><br />
-                        <input type="input" id="price" name="price"></input>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <legend>Event Details</legend>
+          <div>
+            <label>
+              Date
+              <input type="date" id="date" name="date"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Start Time
+              <input type="time" id="startTime" name="startTime"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Start Time
+              <input type="time" id="stopTime" name="stopTime"></input>
+            </label>
+          </div>
+          <div>
+            <label>
+              Price of Event
+              <input type="input" id="price" name="price"></input>
+            </label>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
 
-export default EventDetailsForm
+export default EventDetailsForm;
