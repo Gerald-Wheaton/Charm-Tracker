2092eb26a705c4ec8fd43de50a505372dfea64e9
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-02-24 12:02:08 -0500

    typos were preventing fetches from  the db, form also restricted to wrapping just the input and button

--- charm-tracker/imports/ui/VendorTypeList.js
+++ charm-tracker/imports/ui/VendorTypeList.js
@@ -5,61 +5,60 @@ import { vendorCollection } from "../api/vendors";
 const VendorTypeList = (props) => {
   const { vendorTypeID, vendorTypeName } = props;
 
-  //handles form and adds value to db 
+  //handles form and adds value to db
   const handleSubmit = (event) => {
     event.preventDefault();
     let newVendor = event.target.vendorName.value;
-    console.log(newVendor)
+    console.log(newVendor);
     try {
       if (newVendor) {
         event.target.vendorName.value = "";
-        vendorCollection.insert({  //not actually adding users
+        vendorCollection.insert({
+          //not actually adding users
           createdAt: Date.now(),
           name: newVendor,
           type: vendorTypeID,
         });
-        console.log("vendor added to list")
+        console.log("vendor added to list");
       }
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  }
-
+  };
 
   // get the vendors with the passed in type
-  let vendors = vendorCollection.find({ vendorType: vendorTypeID }).fetch();
-  console.log(vendors)
+  let vendors = vendorCollection.find({ type: vendorTypeID }).fetch();
+  let allvendors = vendorCollection.find({}).fetch();
+  console.log(allvendors);
 
   // return the component
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <fieldset className="vendor">
-          <legend>{vendorTypeName}</legend>
+      <fieldset className="vendor">
+        <legend>{vendorTypeName}</legend>
 
-          {/* render the names of the vendors along with the delete functions for them */}
-          {vendors.map((vendor) => {
-            return (
-              <div key={vendor._id}>
-                <button
-                  onClick={() => {
-                    vendorCollection.remove({ _id: vendor._id });
-                  }}
-                >
-                  X
-                </button>
-                {vendor.name}
-              </div>
-            );
-          })}
+        {/* render the names of the vendors along with the delete functions for them */}
+        {vendors.map((vendor) => {
+          return (
+            <div key={vendor._id}>
+              <button
+                onClick={() => {
+                  vendorCollection.remove({ _id: vendor._id });
+                }}
+              >
+                X
+              </button>
+              {vendor.name}
+            </div>
+          );
+        })}
 
-          {/* input and submit vendor names */}
+        {/* input and submit vendor names */}
+        <form onSubmit={handleSubmit}>
           <input type="text" name="vendorName" />
-          <button>
-            Add {vendorTypeName}
-          </button>
-        </fieldset>
-      </form>
+          <button>Add {vendorTypeName}</button>
+        </form>
+      </fieldset>
     </div>
   );
 };
