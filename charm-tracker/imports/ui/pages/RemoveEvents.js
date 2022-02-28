cb3b1c5b8dd569bc61e3fe5046bc34541764b2d2
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-02-28 01:39:09 -0500

    Added sweetalert2 package and implemented custom comfirmation popups

--- charm-tracker/imports/ui/pages/RemoveEvents.js
+++ charm-tracker/imports/ui/pages/RemoveEvents.js
@@ -1,13 +1,48 @@
-import React from "react"
-import Header from "../Header"
+import React from "react";
+import { eventCollection } from "../../api/events";
+import Header from "../Header";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const RemoveEvents = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let removedVendorType = event.target.removeVendorName.value;
+    if (removedVendorType) {
+      event.target.removeVendorName.value = "";
+      vendorTypeCollection.remove({ _id: removedVendorType });
+      console.log("Vendor Type removed");
+    }
+  };
+
+  // get the vendor types
+  let events = eventCollection.find({}).fetch();
+
   return (
     <div>
       <Header title="Remove Event" />
-      <h1>This is Teh Fricken RemoveEvents</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <p>Select the event you want to remove</p>
+          <select name="removeVendorName" id="removeVendorNames">
+            {events.map((event) => {
+              return (
+                <option key={event._id} value={event._id}>
+                  {event.name}
+                </option>
+              );
+            })}
+          </select>
+          <p>Enter password to confirm removal</p>
+          <input type="text" name="confirmPassword"></input>
+          <Link to="/calendar" className="cancel-button">
+            Cancel
+          </Link>
+          <button>Remove event</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RemoveEvents
+export default RemoveEvents;
