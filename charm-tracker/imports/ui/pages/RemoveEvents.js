16ec25d6684c74f3971b195c31bc35b810171abf
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-02-28 02:06:13 -0500

    set up pop-up with phony password for removing events

--- charm-tracker/imports/ui/pages/RemoveEvents.js
+++ charm-tracker/imports/ui/pages/RemoveEvents.js
@@ -3,16 +3,37 @@ import { eventCollection } from "../../api/events";
 import Header from "../Header";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+//need to create/import user collection
 
 const RemoveEvents = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    let removedVendorType = event.target.removeVendorName.value;
-    if (removedVendorType) {
-      event.target.removeVendorName.value = "";
-      vendorTypeCollection.remove({ _id: removedVendorType });
-      console.log("Vendor Type removed");
-    }
+
+    let userPassword = "Danita";
+    //popup to confirm password
+    const { value: password } = Swal.fire({
+      title: "Enter password to confirm removal",
+      input: "text",
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to write something!";
+        }
+        return new Promise((resolve) => {
+          if (value == userPassword) {
+            let removedEvent = event.target.removeEvent.value;
+            if (removedEvent) {
+              event.target.removeEvent.value = "";
+              eventCollection.remove({ _id: removedEvent });
+              console.log("Event removed");
+            }
+            resolve();
+          } else {
+            resolve("Password was not correct");
+          }
+        });
+      },
+    });
   };
 
   // get the vendor types
@@ -24,7 +45,7 @@ const RemoveEvents = () => {
       <div>
         <form onSubmit={handleSubmit}>
           <p>Select the event you want to remove</p>
-          <select name="removeVendorName" id="removeVendorNames">
+          <select name="removeEvent" id="removeEvent">
             {events.map((event) => {
               return (
                 <option key={event._id} value={event._id}>
@@ -33,12 +54,14 @@ const RemoveEvents = () => {
               );
             })}
           </select>
-          <p>Enter password to confirm removal</p>
-          <input type="text" name="confirmPassword"></input>
-          <Link to="/calendar" className="cancel-button">
-            Cancel
-          </Link>
-          <button>Remove event</button>
+          {/* <p>Enter password to confirm removal</p>
+          <input type="text" name="confirmPassword"></input> */}
+          <div>
+            <Link to="/calendar" className="cancel-button">
+              Cancel
+            </Link>
+            <button>Remove event</button>
+          </div>
         </form>
       </div>
     </div>
