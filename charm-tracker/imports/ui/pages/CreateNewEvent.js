db74aad4373c069677982e164d4b0d7b9850336c
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-03-06 22:13:28 -0500

    create new event form has been refactored to only be one form

--- charm-tracker/imports/ui/pages/CreateNewEvent.js
+++ charm-tracker/imports/ui/pages/CreateNewEvent.js
@@ -31,11 +31,101 @@ const CreateNewEvent = () => {
           <option value="audi">Audi</option>
         </select>
       </div>
-      <ContactDetailsForm previousCustomer={previousCustomer} />
-      <EventDetailsForm />
-      <div>
-        <button>Cancel</button>
-        <button onClick={() => console.log(previousCustomer)}>Add Event</button>
+
+      <div className="ContactDetails">
+        <form>
+          <fieldset className="ContactDetails">
+            <legend>Contact Details</legend>
+            <div>
+              <label>
+                First Name <br />
+                <input
+                  type="input"
+                  id="fname"
+                  name="fname"
+                  defaultValue={previousCustomer}
+                ></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Last Name <br />
+                <input type="input" id="lname" name="lname"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Email <br />
+                <input type="email" id="email" name="email"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Phone Number <br />
+                <input type="input" id="PhoneNum" name="PhoneNum"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Address <br />
+                <input type="input" id="address" name="address"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                City <br />
+                <input type="input" id="city" name="city"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                State <br />
+                <input type="input" id="state" name="state"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Zip <br />
+                <input type="input" id="zip" name="zip"></input>
+              </label>
+            </div>
+          </fieldset>
+
+          <fieldset className="EventDetails">
+            <legend>Event Details</legend>
+            <div>
+              <label>
+                Date <br />
+                <input type="date" id="date" name="date"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Start Time <br />
+                <input type="time" id="startTime" name="startTime"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Start Time <br />
+                <input type="time" id="stopTime" name="stopTime"></input>
+              </label>
+            </div>
+            <div>
+              <label>
+                Price of Event <br />
+                <input type="input" id="price" name="price"></input>
+              </label>
+            </div>
+          </fieldset>
+        </form>
+
+        <div>
+          <button>Cancel</button>
+          <button onClick={() => console.log(previousCustomer)}>
+            Add Event
+          </button>
+        </div>
       </div>
     </div>
   );
