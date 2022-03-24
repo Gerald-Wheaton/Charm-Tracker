9138c3dbd97608dbd3a995c25069259a25d12bcd
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-03-24 10:35:47 -0400

    basic html for reports filter done

--- charm-tracker/imports/ui/pages/Reports.js
+++ charm-tracker/imports/ui/pages/Reports.js
@@ -2,14 +2,46 @@ import React from "react"
 import Header from "../Header"
 
 const Reports = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div>
       <Header title="Reports" />
-      <form>
-        Filter:
-        <input type="radio" name="filter" value="date">Date Range</input>
-        <input type="radio" name="filter" value="name">Name</input>
-        <input type="radio" name="filter" value="both">Both</input>
+      <form onSubmit={handleSubmit}>
+        <p>Filter:</p>
+        <label>
+          <input type="radio" name="filter" value="date"></input>
+          Date
+        </label>
+
+        <label>
+          <input type="radio" name="filter" value="name"></input>
+          Client Name
+        </label>
+
+        <label> 
+          <input type="radio" name="filter" value="both"></input>
+          Both
+        </label>
+
+        <br />
+
+        {/* filters input */}
+        <label className="dateFilter">Start Date:
+          <input type="date" name="startDate"></input>
+        </label>
+        <label className="dateFilter">Stop Date:
+          <input type="date" name="stopDate"></input>
+        </label>
+
+        <label className="nameFilter">Name
+          <input type="text" name="clientName"></input>
+        </label>
+
+        <button>Search</button>
       </form>
     </div>
   )
