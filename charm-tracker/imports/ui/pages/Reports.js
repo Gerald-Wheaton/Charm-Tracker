d365fd07a0e5252b91ebcaa27dc9c13f22fd7000
Emily Port <54337762+emjapo@users.noreply.github.com>
2022-03-24 10:15:44 -0400

    edit event page has been updated to add in the client details too

--- charm-tracker/imports/ui/pages/Reports.js
+++ charm-tracker/imports/ui/pages/Reports.js
@@ -5,7 +5,12 @@ const Reports = () => {
   return (
     <div>
       <Header title="Reports" />
-      <h1>This is Teh Fricken Reports</h1>
+      <form>
+        Filter:
+        <input type="radio" name="filter" value="date">Date Range</input>
+        <input type="radio" name="filter" value="name">Name</input>
+        <input type="radio" name="filter" value="both">Both</input>
+      </form>
     </div>
   )
 }
