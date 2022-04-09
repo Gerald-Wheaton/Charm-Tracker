e5ac2af8f425d4ea631c345c195da94806d6d18b
gerald <gerald.wheaton20@gmail.com>
2022-04-08 21:49:43 -0400

    call tasks for current day

--- charm-tracker/imports/ui/pages/Calendar/TasksSidebar.js
+++ charm-tracker/imports/ui/pages/Calendar/TasksSidebar.js
@@ -2,12 +2,25 @@ import React from "react"
 import GenerateTasksFromEvent from "../../../api/taskHandling/TaskHandler.js"
 import { taskCollection } from "../../../api/tasks"
 import { eventCollection } from "../../../api/events"
+//import { haveData, dueToday } from "./taskFunctions.js"
+
+const haveData = data => {
+  return data.length !== 0 ? true : false
+}
 
 const TasksSidebar = props => {
   //TODOs: call below function based on events with due dates corelating to today
   //GenerateTasksFromEvent()
-  const task = taskCollection.find({}).fetch()
-  console.log(task)
+  const task = taskCollection
+    .find({
+      /*dueDate: today's Date*/
+    })
+    .fetch()
+
+  if (haveData(task)) {
+    console.log(task)
+  }
+  //{`Due today: ${task[0].task}`}
   return <></> //<>{`Hello ${task[0]}`}</>
 }
 
