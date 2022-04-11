a5191b7628974c05bf2711ae35cdf2d90655de59
gerald <gerald.wheaton20@gmail.com>
2022-04-10 23:39:37 -0400

    fix date math

--- charm-tracker/imports/api/taskHandling/remindMethods.js
+++ charm-tracker/imports/api/taskHandling/remindMethods.js
@@ -5,25 +5,24 @@ import React from "react"
 // dueDate = dueDate.split(",")
 // dueDate = dueDate[0]
 
+// subtract offset of one day (86400000) in milliseconds
+// this is due to date formatting conflicts.
 const remind = {
   onFourteen: date => {
     let dueDate = Date.parse(date)
-    dueDate = dueDate - 1209600000
-    dueDate = Date(dueDate)
+    dueDate = dueDate - (1209600000 - 86400000)
     const due = new Date(dueDate).toLocaleString().split(",")
     return due[0]
   },
   onThirty: date => {
     let dueDate = Date.parse(date)
-    dueDate = dueDate - 2592000000
-    dueDate = Date(dueDate)
+    dueDate = dueDate - (2592000000 - 86400000)
     const due = new Date(dueDate).toLocaleString().split(",")
     return due[0]
   },
   onSixty: date => {
     let dueDate = Date.parse(date)
-    dueDate = dueDate - 5184000000
-    dueDate = dueDate.toString() //Date(dueDate)
+    dueDate = dueDate - (5184000000 - 86400000)
     const due = new Date(dueDate).toLocaleString().split(",")
     return due[0]
   },
