afb81f59447b461c68d9ee044b3ce8a638faf159
gerald <gerald.wheaton20@gmail.com>
2022-04-14 03:20:22 -0400

    More progress

--- charm-tracker/imports/ui/Day/Day.js
+++ charm-tracker/imports/ui/Day/Day.js
@@ -2,25 +2,30 @@ import { ConstructionOutlined } from "@mui/icons-material"
 import React from "react"
 import { eventCollection } from "../../api/events"
 
+const Event = title => {
+  console.log(title)
+  return <div className="event">{title.title}</div>
+}
+
 export const Day = ({ day, onClick, evtsToday }) => {
   const className = `day ${day.value === "padding" ? "padding" : ""} ${
     day.isCurrentDay ? "currentDay" : ""
   }`
 
   // if (evtsToday.length != 0) {
-  //   console.log(evtsToday.map((e, index) => e.email))
-  // } else {
-  //   console.log("")
+  //   evtsToday.map((e, index) => console.log(e.email))
   // }
   return (
     <div onClick={onClick} className={className}>
       {day.value === "padding" ? "" : day.value}
 
-      {evtsToday.length != 0 && (
-        <div className="event">
-          {evtsToday.map((e, index) => e.name.firstName)}
+      {evtsToday.length !== 0 ? (
+        <div>
+          {React.Children.toArray(
+            evtsToday.map((e, index) => <Event title={e.email} />)
+          )}
         </div>
-      )}
+      ) : null}
     </div>
   )
 }
